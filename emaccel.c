66cbc95a351012801b48f82f9dfb3eabcf680439
ANGSD <thorfinn.sand@gmail.com>
2017-07-01 22:10:51 +0100

    initial version should work for testing

--- emaccel.c
+++ emaccel.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <assert.h>
-
+#include <time.h>
 
 typedef struct{
   size_t l;
@@ -17,13 +17,15 @@ typedef struct{
 
 typedef struct{
   emaccl_internal *internal;//keeps track of internal variable and allocations
-  double (*llh)(double *,void *);//llh function. Log likelihood. This should be >0. and we seek the parameters that minimizes this function.
-  double (*emstep)(double *,void*,double*);//function that returns the next guess.
+  double (*llhFP)(double *,void *);//llh function. Log likelihood. This should be >0. and we seek the parameters that minimizes this function.
+  int (*emstepFP)(double *,void*,double*);//function that returns the next guess.
   int verbose;//should we printout stuff
-  double ttol;//tolerance
+  double ttol;//tolerance used in accelerated em
+  double tole;//tolerance used for difference in llh values
   double l;//dimension of parameterspase
   double type;//type=0 normal em, type=1 accelerated em.
-}empars;
+  double maxIter;
+}emaccl_pars;
 
 
 typedef struct{
@@ -32,18 +34,20 @@ typedef struct{
 }testp;
 
 
-double llh(double f,double *d,int n){
+double llh(double *f,void *vp){
+  testp *p =(testp*) vp;
   double llh=0;
-  for(int i=0;i<n;i++)
-    llh += log( f*exp(d[i*2])+(1-f)*exp(d[i*2+1]));
+  for(int i=0;i<p->n;i++)
+    llh += log( f[0]*exp(p->d[i*2])+(f[1])*exp(p->d[i*2+1]));
   return -llh;
 }
 
-int emstep(double *pre,double *post,double *d,int n){
+int emstep(double *pre,void *vp,double *post){
+  testp *p =(testp*) vp;
   double f2=0;
-   for(int i=0;i<n;i++)
-     f2 += pre[0]*exp(d[i*2])/(pre[0]*exp(d[i*2])+pre[1]*exp(d[2*i+1]));
-   post[0] = f2/(1.0*n);
+   for(int i=0;i<p->n;i++)
+     f2 += pre[0]*exp(p->d[i*2])/(pre[0]*exp(p->d[i*2])+pre[1]*exp(p->d[2*i+1]));
+   post[0] = f2/(1.0*p->n);
    post[1] = 1-post[0];
    //   fprintf(stderr,"diff1:%e\n",1-post[0]-post[1]);
    return 0;
@@ -62,41 +66,39 @@ emaccl_internal *emaccl_internal_alloc(int ndim){
   return ret;
 }
 
-#if 0
-
-int em1(double *sfs,double  **emis,double tole,int maxIter,int len,int verbose){
-  double oldLik,lik;
-  oldLik = loglike(sfs,emis,len);
-  if(verbose)
-    fprintf(stderr,"startlik=%f %f %f %f\n",oldLik,sfs[0],sfs[1],sfs[2]);
-  fflush(stderr);
-
-  double tmp[3];
-  int it;
-  for(it=0;it<maxIter;it++) {
-    emStep1(sfs,emis,tmp,len);
-    for(int i=0;i<3;i++)
-      sfs[i]= tmp[i];
-    lik = loglike(sfs,emis,len);
-
-    if(verbose)
-      fprintf(stderr,"[%d] lik=%f diff=%g\n",it,lik,fabs(lik-oldLik));
-
-    if(fabs(lik-oldLik)<tole){
-     
-      oldLik=lik;
-      break;
-    }
-    oldLik=lik;
-  }
-  return it;
+emaccl_pars *emaccl_pars_alloc(int ndim){
+  emaccl_pars *ep = malloc(sizeof(emaccl_pars));
+  ep->internal = emaccl_internal_alloc(ndim);
+  ep->ttol=1e-9;
+  ep->tole=1e-6;
+  ep->l=ndim;
+  ep->type=1;
+  ep->maxIter = 100;
+  ep->llhFP=NULL;
+  ep->emstepFP=NULL;
+  ep->verbose=100;
+  return ep;
 }
-#endif
 
-#if 0
-int emAccel(double *F,double **emis,double *F_new,int len){
+void minus(double *fst,double *sec,double *res,int l){
+  for(int i=0;i<l;i++)
+    res[i] = fst[i]-sec[i];
+}
+
+double sumSquare(double *mat, int l){
+  double tmp=0;
+  for(size_t i=0;i<3;i++){
+    //    fprintf(stderr,"%f \n",mat[i]);
+    tmp += mat[i]*mat[i];
+  }
+
+  return tmp;
+}
+
+
+int emAccel(double *F,void *vp,double *F_new,emaccl_pars *ep){
   //  fprintf(stderr,"calling emaccel \n");
-   double ttol=0.0000001;
+  // double ttol=0.0000001;
 
   //  fprintf(stderr,"tol:%f\n",tol);
   //maybe these should be usersettable?
@@ -107,62 +109,64 @@ int emAccel(double *F,double **emis,double *F_new,int len){
   //  double objfnInc=1;
 
 
-  double F_em1[3];
-  double F_diff1[3];
-  double F_em2[3];
-  double F_diff2[3];
-  double F_diff3[3];
-  double F_tmp[3];
+  double *F_em1=ep->internal->F_em1;
+  double *F_em2=ep->internal->F_em2;
+  double *F_diff1=ep->internal->F_diff1;
+  double *F_diff2=ep->internal->F_diff2;
+  double *F_diff3=ep->internal->F_diff3;
+  double *F_tmp=ep->internal->F_tmp;
 
-  emStep1(F,emis,F_em1,len);
+  ep->emstepFP(F,vp,F_em1);
   // stayin(F_em1);
-  minus(F_em1,F,F_diff1);
-  double sr2 = sumSquare(F_diff1);
+  minus(F_em1,F,F_diff1,ep->l);
+  double sr2 = sumSquare(F_diff1,ep->l);
   
-  if(sqrt(sr2)<ttol){
-    //    fprintf(stderr,"sr2 break:%f\n",sr2);
+  if(sqrt(sr2)<ep->ttol){
+    fprintf(stderr,"sr2 break:%f\n",sr2);
     return 0;
     //break;
   }
-  emStep1(F_em1,emis,F_em2,len);
-  //  stayin(F_em2);
-  minus(F_em2,F_em1, F_diff2);
+  ep->emstepFP(F_em1,vp,F_em2);
+  minus(F_em2,F_em1, F_diff2,ep->l);
 
-  double sq2 = sumSquare(F_diff2);
-  if(sqrt(sq2)<ttol){
-    //fprintf(stderr,"sq2\n");
+  double sq2 = sumSquare(F_diff2,ep->l);
+  if(sqrt(sq2)<ep->ttol){
+    fprintf(stderr,"sq2\n");
     return 0;
     //    break;
   }
 
 
-  minus(F_diff2,F_diff1, F_diff3);
+  minus(F_diff2,F_diff1, F_diff3,ep->l);
   
-  double sv2 = sumSquare(F_diff3);
+  double sv2 = sumSquare(F_diff3,ep->l);
   
   double alpha = sqrt(sr2/sv2);
-  alpha = std::max(stepMin,std::min(stepMax,alpha));
-  for(size_t i=0;i<3;i++)
+  double mmin=stepMax<alpha?stepMax:alpha;
+  alpha =stepMin>mmin?stepMin:mmin;// ;std::max(stepMin,std::min(stepMax,alpha));
+  for(size_t i=0;i<ep->l;i++)
     F_new[i] = F[i]+2*alpha*F_diff1[i]+alpha*alpha*F_diff3[i];
-  //  fprintf(stderr,"F_new (this the linear jump: (%f,%f,%f)\n",F_new[0],F_new[1],F_new[2]);
+  //  fprintf(stderr,"F_new (this the linear jump: (%f,%f)\n",F_new[0],F_new[1]);
   int outofparspace =0;
-  for(int i=0;i<3;i++){
+  for(int i=0;i<ep->l;i++){
     if(F_new[i]<0||F_new[i]>1)
       outofparspace++;
   }
   if(outofparspace){
-    //    fprintf(stderr,"outofparspace will use second emstep as jump\n");
-    for(int i=0;i<3;i++)
+    fprintf(stderr,"outofparspace will use second emstep as jump\n");
+    for(int i=0;i<ep->l;i++)
       F_new[i] = F_em2[i];
     return 1;
   }
     
   
   if (fabs(alpha - 1) > 0.01){
-    emStep1(F_new,emis,F_tmp,len);
-    //    stayin(F_tmp);
-    for(int i=0;i<3;i++)
-      std::swap(F_new[i],F_tmp[i]);
+    ep->emstepFP(F_new,vp,F_tmp);
+    for(int i=0;i<ep->l;i++){
+      double tmp = F_new[i];
+      F_new[i]=F_tmp[i];
+      F_tmp[i]=tmp;
+    }
   }
 
   //  double lnew = 1;
@@ -175,11 +179,60 @@ int emAccel(double *F,double **emis,double *F_new,int len){
   // fprintf(stderr,"calling emaccel \n");
   // fprintf(stderr,"F_new:(%f,%f,%f)\n",F_new[0],F_new[1],F_new[2]);
   return 1;
-
-  
 }
 
-#endif
+
+
+int em1(double *sfs,void *vpp,void *vp){
+  emaccl_pars *ep = vpp;
+  assert(ep->llhFP);assert(ep->emstepFP);
+  double oldLik,lik;
+  oldLik = ep->llhFP(sfs,vp);
+  if(ep->verbose){
+    fprintf(stderr,"startlik=%f",oldLik);
+    for(int i=0;i<ep->l;i++)
+      fprintf(stderr,"\t%f",sfs[i]);
+    fprintf(stderr,"\n");
+  }
+  fflush(stderr);
+
+  double *tmp =malloc(ep->l*sizeof(double));
+  int it;
+  for(it=0;it<ep->maxIter;it++) {
+    if(ep->type==0)
+      ep->emstepFP(sfs,vp,tmp);
+    else if(ep->type==1)
+      emAccel(sfs,vp,tmp,ep);
+    lik = ep->llhFP(tmp,vp);
+    
+    double pdif=0;
+    for(int i=0;i<ep->l;i++)
+      pdif +=(tmp[i]-sfs[i])*(tmp[i]-sfs[i]);
+    if(ep->verbose){
+      fprintf(stderr,"[%d] lik=%f diff_llh=%g sqrt(diff_pars):%e ",it,lik,lik-oldLik,sqrt(pdif));
+      for(int i=0;i<ep->l;i++)
+	fprintf(stderr,"\t %e",tmp[i]-sfs[i]);
+      fprintf(stderr,"\n");
+    }
+    if(lik>oldLik){
+      fprintf(stderr,"\t-> New likelihood from EM-step is worse, assuming optima has been achieved and we are observing an issue of numerical stability\n");
+      break;
+    }else{
+      for(int i=0;i<3;i++)
+	sfs[i]= tmp[i];
+      assert(lik<=oldLik);
+      if(lik-oldLik==0||fabs(lik-oldLik)<ep->tole){
+	if(ep->verbose)
+	  fprintf(stderr,"Breaking in emalgo, since difference in llh are smaller than tolerance\n");
+	oldLik=lik;
+	break;
+      }
+      oldLik=lik;
+    }
+  }
+  return it;
+}
+
 
 void mean(int *obs,int n){
  double s=0;
@@ -223,6 +276,9 @@ double *simdata(double f,int n,double errate,int dep){
 }
 
 int main(){
+  clock_t t=clock();
+  time_t t2=time(NULL);
+
   double f=0.000018;
   double err = 0.025;
   int dep =3;
@@ -232,20 +288,28 @@ int main(){
     fprintf(stdout,"%f\t%f\n",d[i*2],d[i*2+1]);
   fflush(stdout);
   double start[2] = {0.5,0.5};
-  double oldlik  =llh(start[0],d,n);
+  testp tp;
+  tp.d=d;tp.n=n;
+  double oldlik  =llh(start,&tp);
   fprintf(stderr,"(%f,%f):llh1:%f\n",start[0],start[1],oldlik);
-  double tmp[2];
-  for(int i=0;i<10000;i++){
-    emstep(start,tmp,d,n);
-    double newllh = llh(tmp[0],d,n);
-    fprintf(stderr,"[%d]:(%f,%f):llh2:%f diff:%e\n",i,tmp[0],tmp[1],newllh,newllh-oldlik);
-    assert(newllh<=oldlik);
-    start[0]=tmp[0];start[1]=tmp[1];
-    if(newllh-oldlik==0)
-      break;
 
-    oldlik=newllh;
-  }
+  emaccl_pars *ep=emaccl_pars_alloc(2);
+  ep->llhFP=llh;
+  ep->emstepFP=emstep;
+  ep->type =0;
+  em1(start,ep,&tp);
+
+  fprintf(stderr, "\t[ALL done] cpu-time used =  %.2f sec\n", (float)(clock() - t) / CLOCKS_PER_SEC);
+  fprintf(stderr, "\t[ALL done] walltime used =  %.2f sec\n", (float)(time(NULL) - t2));  
+
+  t=clock();
+  t2=time(NULL);
+
+  start[0]=start[1]=0.5;
+  ep->type =1;
+  em1(start,ep,&tp);
+  fprintf(stderr, "\t[ALL done] cpu-time used =  %.2f sec\n", (float)(clock() - t) / CLOCKS_PER_SEC);
+  fprintf(stderr, "\t[ALL done] walltime used =  %.2f sec\n", (float)(time(NULL) - t2));  
       
   return 0;
 }
